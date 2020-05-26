a7d854cc8452da01238f78caf949f8f469398774
Will <waormd@gmail.com>
2020-05-26 16:53:03 +0100

    Display index page with list items

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items = session.get_items())
 
 if __name__ == '__main__':
     app.run()
