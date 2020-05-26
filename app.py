d6110bf0dfd801f207bd0d22005c26f90f7866cf
Will <waormd@gmail.com>
2020-05-26 17:19:55 +0100

    Add post method to add new items

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        item = request.form['item']
+        session.add_item(item)
     return render_template('index.html', items = session.get_items())
 
 if __name__ == '__main__':
