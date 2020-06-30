86fb7e575bfe3bc17e2d0a4e6da458367af7652c
Will Cole-Jordan <waormd@gmail.com>
2020-06-30 14:11:44 +0100

    split GET and POST into separate methods

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        item = request.form['item']
-        session.add_item(item)
-    return render_template('index.html', items = session.get_items())
+@app.route('/', methods=['POST'])
+def indexPost():
+    item = request.form['item']
+    session.add_item(item)
+    return index(session.get_items())
+
+@app.route('/', methods=['GET'])
+def indexGet():
+    return index(session.get_items())
+
+def index(sessionItems):
+    return render_template('index.html', items = sessionItems)
+
 
 if __name__ == '__main__':
     app.run()
