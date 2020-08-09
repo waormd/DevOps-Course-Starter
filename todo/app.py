a62d734dac18f4d767cfeb172406434ff0ffb2a7
Will Cole-Jordan <waormd@gmail.com>
2020-08-09 16:03:47 +0100

    move trello calls to trello api class

--- todo/app.py
+++ todo/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from todo.item import Item
 from todo.view import ViewModel
+from todo.trello_api import TrelloApi
 import os
 
 import requests as r
@@ -9,76 +10,53 @@ import yaml
 TRELLO_BASE_URL = 'https://api.trello.com'
 TRELLO_BOARD_ID = os.getenv('TRELLO_BOARD_ID')
 
-app = Flask(__name__)
-
-def create_app():
-    app = Flask(__name__)
-    return app
-
-@app.route('/', methods=['POST'])
-def indexPost():
-    title = request.form['item']
-    secrets = loadSecrets()
-    lists = loadLists(secrets, TRELLO_BOARD_ID)
-    for item in lists:
-        if item['name'] == "Todo":
-            addCard(secrets, item['id'], title)
-    return redirect(url_for('indexGet'))
-
-@app.route('/', methods=['GET'])
-def indexGet():
-    secrets = loadSecrets()
-    lists = loadLists(secrets, TRELLO_BOARD_ID)
-    item_view_model = ViewModel(loadItems(secrets, lists))
-    return render_template('index.html', view_model = item_view_model)
-
-@app.route('/complete_item', methods=['POST'])
-def indexPut():
-    cardId = request.form['cardId']
-    target = request.form['target']
-    secrets = loadSecrets()
-    lists = loadLists(secrets, TRELLO_BOARD_ID)
-    for item in lists:
-        if item['name'] == target:
-            moveCard(secrets, cardId, item['id'])
-    return redirect(url_for('indexGet'))
-
 def loadSecrets():
     with open('config/secrets.yml') as file:
         secrets = yaml.load(file, Loader=yaml.FullLoader)
         return secrets['trello']
     return {}
 
-def loadItems(secrets, lists):
+app = Flask(__name__)
+trelloApi = TrelloApi(TRELLO_BASE_URL, loadSecrets())
+
+def create_app():
+    app = Flask(__name__)
+    return app, trelloApi
+
+@app.route('/', methods=['POST'])
+def indexPost():
+    title = request.form['item']
+    lists = trelloApi.loadLists(TRELLO_BOARD_ID)
+    for item in lists:
+        if item['name'] == "Todo":
+            trelloApi.addCard(item['id'], title)
+    return redirect(url_for('indexGet'))
+
+@app.route('/', methods=['GET'])
+def indexGet():
+    lists = trelloApi.loadLists(TRELLO_BOARD_ID)
+    item_view_model = ViewModel(loadItems(lists))
+    return render_template('index.html', view_model = item_view_model)
+
+@app.route('/complete_item', methods=['POST'])
+def indexPut():
+    cardId = request.form['cardId']
+    target = request.form['target']
+    lists = trelloApi.loadLists(TRELLO_BOARD_ID)
+    for item in lists:
+        if item['name'] == target:
+            trelloApi.moveCard(cardId, item['id'])
+    return redirect(url_for('indexGet'))
+
+
+def loadItems(lists):
     flattened = []
     for item in lists:
-        cards = loadCards(secrets, item['id'])
+        cards = trelloApi.loadCards(item['id'])
         for card in cards:
             flattened.append(Item(card['id'], item['name'], card['name'], card['dateLastActivity']))
     return flattened
 
-
-def loadLists(secrets, boardId):
-    url = f'{TRELLO_BASE_URL}/1/boards/{boardId}/lists?key={secrets["api-key"]}&token={secrets["server-token"]}'
-    print(f'GET to {url}')
-    return r.get(url).json()
-
-def loadCards(secrets, listId):
-    url = f'{TRELLO_BASE_URL}/1/lists/{listId}/cards?key={secrets["api-key"]}&token={secrets["server-token"]}'
-    print(f'GET to {url}')
-    return r.get(url).json()
-
-def addCard(secrets, listId, name):
-    url = f'{TRELLO_BASE_URL}/1/cards?key={secrets["api-key"]}&token={secrets["server-token"]}&idList={listId}&name={name}'
-    print(f'POST to {url}')
-    return r.post(url)
-
-def moveCard(secrets, cardId, targetListId):
-    url = f'{TRELLO_BASE_URL}/1/cards/{cardId}?key={secrets["api-key"]}&token={secrets["server-token"]}&idList={targetListId}'
-    print(f'PUT to {url}')
-    return r.put(url)
-
-
 if __name__ == '__main__':
-    app = create_app()
+    app, trelloApi = create_app()
     app.run()
---
+++ todo/trello_api.py
@@ -0,0 +1,27 @@
+import requests as r
+
+class TrelloApi:
+
+    def __init__(self, url, secrets):
+        self.url = url
+        self.secrets = secrets
+
+    def loadLists(self, boardId):
+        url = f'{self.url}/1/boards/{boardId}/lists?key={self.secrets["api-key"]}&token={self.secrets["server-token"]}'
+        print(f'GET to {url}')
+        return r.get(url).json()
+
+    def loadCards(self, listId):
+        url = f'{self.url}/1/lists/{listId}/cards?key={self.secrets["api-key"]}&token={self.secrets["server-token"]}'
+        print(f'GET to {url}')
+        return r.get(url).json()
+
+    def addCard(self, listId, name):
+        url = f'{self.url}/1/cards?key={self.secrets["api-key"]}&token={self.secrets["server-token"]}&idList={listId}&name={name}'
+        print(f'POST to {url}')
+        return r.post(url)
+
+    def moveCard(self, cardId, targetListId):
+        url = f'{self.url}/1/cards/{cardId}?key={self.secrets["api-key"]}&token={self.secrets["server-token"]}&idList={targetListId}'
+        print(f'PUT to {url}')
+        return r.put(url)
