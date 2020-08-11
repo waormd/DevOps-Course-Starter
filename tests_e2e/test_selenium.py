fe513159496610d01de7dae8fedab6f11c6a505f
Will Cole-Jordan <waormd@gmail.com>
2020-08-11 16:17:50 +0100

    Commit stuff before resetting windows

--- tests_e2e/test_selenium.py
+++ tests_e2e/test_selenium.py
@@ -2,6 +2,8 @@ import todo.app as app
 import requests as r
 import pytest
 import os
+import yaml
+from todo.trello_api import TrelloApi
 from threading import Thread
 from selenium import webdriver
 
@@ -12,12 +14,20 @@ def createBoard(key, token, boardName):
 def deleteBoard(key, token, boardId):
     r.delete(f"https://api.trello.com/1/boards/{boardId}?key={key}&token={token}")
 
+def loadSecrets():
+    with open('config/secrets.yml') as file:
+        secrets = yaml.load(file, Loader=yaml.FullLoader)
+        return secrets['trello']
+    return {}
+
+
 @pytest.fixture(scope='module')
 def test_app():
     board_id = createBoard('key', 'token', 'testBoard')
     os.environ['TRELLO_BOARD_ID'] = board_id
-    
-    application = app.create_app()
+
+    trello_api = TrelloApi('https://api.trello.com', loadSecrets())
+    application = app.create_app(trello_api)
 
     thread = Thread(target=lambda: application.run(use_reloader=False))
     thread.daemon = True
@@ -29,7 +39,7 @@ def test_app():
 
 @pytest.fixture(scope="module")
 def driver():
-    with webdriver.Firefox() as driver:
+    with webdriver.Firefox(executable_path='C:\\tools\\selenium\\drivers\\geckodriver.exe') as driver:
         yield driver
 
 def test_task_journey(driver, test_app):
